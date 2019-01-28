5067930b02b74099a586059a6736d5696c151ec9
Matthew Burke <matthewpatrickburke@gmail.com>
2019-01-28 10:18:07 -0700

    Finished pipeline notes.

--- scikit-learn-tutorial/text-2-pipeline.py
+++ scikit-learn-tutorial/text-2-pipeline.py
@@ -3,7 +3,10 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import GridSearchCV
 import numpy as np
+
+# https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
 
 # Training pipeline
 
@@ -19,6 +22,8 @@ twenty_train = fetch_20newsgroups(
 	categories = categories,
 	shuffle = True)
 
+print(twenty_train.data)
+
 text_clf = Pipeline([
 	('vect', CountVectorizer()),
 	('tfidf', TfidfTransformer()),
@@ -26,7 +31,7 @@ text_clf = Pipeline([
 	('clf', SGDClassifier())
 	])
 
-text_clf.fit(twenty_train.data, twenty_train.target)
+# text_clf.fit(twenty_train.data, twenty_train.target)
 
 # Testing
 
@@ -35,7 +40,18 @@ twenty_test = fetch_20newsgroups(
 	categories = categories,
 	shuffle = True)
 
-predictions = text_clf.predict(twenty_test.data)
-print(np.mean(predictions == twenty_test.target))
+# predictions = text_clf.predict(twenty_test.data)
+# print(np.mean(predictions == twenty_test.target))
 
 # Parameter tuning using grid search
+
+pg = {
+	'vect__ngram_range': [(1, 1), (1, 2)],
+	'tfidf__use_idf': (True, False),
+	'clf__alpha': (1e-2, 1e-3)
+}
+
+gs_clf = GridSearchCV(text_clf, pg, cv = 5, iid = False, n_jobs = -1)
+gs_clf.fit(twenty_train.data[:400], twenty_test.target[:400])
+# This seems unreliable!
+print(twenty_train.target_names[gs_clf.predict(['God is love'])[0]])
